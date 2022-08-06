4c81cd27ac5e5e0e4ea6bd4e222cc32af3839c66
Danya <theincredibledanzel@yandex.ru>
2022-08-06 18:23:39 +0400

    code edit

--- Task_Program/Program.cs
+++ Task_Program/Program.cs
@@ -1,9 +1,13 @@
 ﻿string[] stringArray = {"flower", "bus", "coffee", "777", "laptop", "AIO", "b2b", "12", "Samara", "NB", "NorthBridge", "Qiwi", "X", "AMD", "Intel", "SuperFlower"};
 string bufer = string.Empty;
-//int[] indexesArray = new int[];
+int indexesCounter = 0;
 for(int i = 0; i < stringArray.Length; i++)
 {
-    int indexesCounter = 0;
-    
+    bufer = stringArray[i];
+    if(bufer.Length <= 3)
+    {
+        indexesCounter++;
+    }
+    bufer = string.Empty;
 }
-
+int[] indexesArray = new int[indexesCounter];
