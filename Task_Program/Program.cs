22110d8f605065c25ef1404fa8e79442d7e17a03
Danya <theincredibledanzel@yandex.ru>
2022-08-06 18:15:26 +0400

    added first code

--- Task_Program/Program.cs
+++ Task_Program/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] stringArray = {"flower", "bus", "coffee", "777", "laptop", "AIO", "b2b", "12", "Samara", "NB", "NorthBridge", "Qiwi", "X", "AMD", "Intel", "SuperFlower"};
+string bufer = string.Empty;
+//int[] indexesArray = new int[];
+for(int i = 0; i < stringArray.Length; i++)
+{
+    int indexesCounter = 0;
+    
+}
+
